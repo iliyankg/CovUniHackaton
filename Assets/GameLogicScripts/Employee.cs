dffa93279f56adad447234b3620759b33434d70d
Iliyan Georgiev <iliyankgeorgiev@gmail.com>
2016-03-18 19:55:41 +0000

    Begun AI / Game logic implementation

---
+++ Assets/GameLogicScripts/Employee.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Employee {
+
+    public string emp_name;
+    public float dailyMoneyContribution;
+
+    EnumGood[] good_acts;
+    EnumBad[] bad_acts;
+
+    public Employee (string name, EnumGood[] good, EnumBad[] bad, float expectedContribution)
+    {
+        emp_name = name;
+        good_acts = good;
+        bad_acts = bad;
+        dailyMoneyContribution = expectedContribution - (0.1f * (bad.Length - 1) * expectedContribution);
+    }
+
+}
---
+++ Assets/GameLogicScripts/EmployeeFactory.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EnumGood
+{
+    invalid = -1,
+    fileroom,
+    presenting, 
+    pcwork, 
+    photocopy,
+    meeting,
+    cleaning,
+    num_of_good
+}
+
+public enum EnumBad
+{
+    invalid = -1, 
+    nutflix,
+    chatting,
+    socialmedia,
+    gaming,
+    num_of_good
+}
+
+public class EmployeeFactory
+{
+    EmployeeFactory() { }
+
+    public Employee CreateEmployee()
+    {
+
+    }
+
+}
