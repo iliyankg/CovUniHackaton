150226f61a6fd2666eeb1c4043b5fd2d3c6437d5
Iliyan Georgiev <iliyankgeorgiev@gmail.com>
2016-03-18 22:50:52 +0000

    Added Employee and Employee Factory scripts
    TO DO:
    Implement anim swtich
    Implement Add / Take money

--- Assets/GameLogicScripts/Employee.cs
+++ Assets/GameLogicScripts/Employee.cs
@@ -1,20 +1,91 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Employee {
+public class Employee : MonoBehaviour
+{
 
     public string emp_name;
     public float dailyMoneyContribution;
+    public float isGoodPercentage = 50;
 
-    EnumGood[] good_acts;
-    EnumBad[] bad_acts;
+    public EnumGood[] good_acts;
+    public EnumBad[] bad_acts;
 
-    public Employee (string name, EnumGood[] good, EnumBad[] bad, float expectedContribution)
+    int currentAction = -1;
+
+    public void Populate (string name, EnumGood[] good, EnumBad[] bad, float expectedContribution)
     {
         emp_name = name;
         good_acts = good;
         bad_acts = bad;
-        dailyMoneyContribution = expectedContribution - (0.1f * (bad.Length - 1) * expectedContribution);
+        dailyMoneyContribution = expectedContribution - (0.1f * (bad.Length) * expectedContribution);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Action());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Action()
+    {
+        bool isGood;
+
+        if (Random.Range(0f, 100f) > 100 - isGoodPercentage)
+            isGood = true;
+        else
+            isGood = false;
+
+        //change anim and sound
+
+        if (isGood)
+        {
+            currentAction = Random.Range(0, good_acts.Length - 1);
+            switch((EnumGood)currentAction)
+            {
+                case EnumGood.fileroom:
+                    break;
+                case EnumGood.skype:
+                    break;
+                case EnumGood.pcwork:
+                    break;
+                case EnumGood.photocopy:
+                    break;
+            }
+        }
+        else if(!isGood)
+        {
+            currentAction = Random.Range(0, bad_acts.Length - 1);
+            switch ((EnumBad)currentAction)
+            {
+                case EnumBad.nutflix:
+                    break;
+                case EnumBad.chatting:
+                    break;
+                case EnumBad.socialmedia:
+                    break;
+                case EnumBad.gaming:
+                    break;
+                case EnumBad.onphone:
+                    break;
+                case EnumBad.fapfap:
+                    break;
+            }
+        }
+
+        
+
+        yield return new WaitForSeconds(Random.Range(5f, 15f));
+
+        //make/take money
+
+        StartCoroutine(Action());
     }
 
 }
