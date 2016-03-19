97efca807ccbd8fde27fd44a1afd7ec1035671d6
Gustavo <mingalll@yahoo.com.br>
2016-03-19 08:07:27 +0000

    sono

---
+++ Assets/Scripts/AndroidTiltScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndroidTiltScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Input.gyro.enabled = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(Input.gyro.rotationRateUnbiased.x, Input.gyro.rotationRateUnbiased.y, Input.gyro.rotationRateUnbiased.z);
+        //transform.Rotate(0f, 1f, 0f);
+	
+	}
+}
